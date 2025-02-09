aba6da5ff02e9b727aacbf951e2e4ddfa9f6594f
Angelo Meza <meza1044@gmail.com>
2025-02-09 17:15:59 -0500

    Creación de Dtos.

--- CustomerBackend.Application/DTOs/CompanyDTO.cs
+++ CustomerBackend.Application/DTOs/CompanyDTO.cs
@@ -6,31 +6,24 @@ using System.Threading.Tasks;
 
 namespace CustomerBackend.Application.DTOs
 {
-    public class CustomerDTO
+    public class CompanyDTO
     {
-        public long Id { get; set; }
-
         public long CompanyId { get; set; }
 
-        public string FirstName { get; set; }
+        public string CompanyName { get; set; }
 
-        public string LastName { get; set; }
+        public string TaxId { get; set; }
 
-        public string Email { get; set; }
+        public string Address { get; set; }
 
         public string Phone { get; set; }
 
-        public string Address { get; set; }
+        public string Email { get; set; }
 
         public DateTime CreatedDate { get; set; }
 
         public DateTime? UpdatedDate { get; set; }
 
         public bool IsActive { get; set; }
-
-        // Propiedades adicionales
-        public string CompanyName { get; set; }
-
-        public string FullName => $"{FirstName} {LastName}";
     }
 }
--- CustomerBackend.Application/DTOs/UpdateCustomerDTO.cs
+++ CustomerBackend.Application/DTOs/UpdateCustomerDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace CustomerBackend.Application.DTOs
 {
     public class UpdateCustomerDTO
     {
+        [Required(ErrorMessage = "El ID es obligatorio")]
+        public long Id { get; set; }
+
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 100 caracteres")]
+        public string FirstName { get; set; }
+
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "El apellido debe tener entre 2 y 100 caracteres")]
+        public string LastName { get; set; }
+
+        [EmailAddress(ErrorMessage = "El formato del email no es válido")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "El formato del teléfono no es válido")]
+        public string Phone { get; set; }
+
+        [StringLength(200, ErrorMessage = "La dirección no puede exceder los 200 caracteres")]
+        public string Address { get; set; }
     }
 }
